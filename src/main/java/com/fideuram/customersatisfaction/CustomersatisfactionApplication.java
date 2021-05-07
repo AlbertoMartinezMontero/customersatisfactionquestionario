f5f798843cf7a395c3394808a664e1c7d2081dd5
AlbertoMartinezMontero <79788629+AlbertoMartinezMontero@users.noreply.github.com>
2021-05-07 09:41:07 +0200

    architettura Spring

---
+++ src/main/java/com/fideuram/customersatisfaction/CustomersatisfactionApplication.java
@@ -0,0 +1,13 @@
+package com.fideuram.customersatisfaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersatisfactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersatisfactionApplication.class, args);
+	}
+
+}
